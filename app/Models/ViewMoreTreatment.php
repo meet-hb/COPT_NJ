5c65718180b40f9143e01c7b410cc51eed3fa34f
HackberrySoftech <hackberrysoftech@gmail.com>
2025-04-14 12:20:48 +0530

    Add new Models + Migrations

---
+++ app/Models/ViewMoreTreatment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ViewMoreTreatment extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2025_04_14_064919_create_treatment_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatment_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('treatment_id');
+            $table->string('type', 255);
+            $table->text('description');
+            $table->boolean('is_active')->default(1)->comment('1=Active, 0=Inactive');
+            $table->boolean('is_deleted')->default(0)->comment('1=Deleted, 0=Not Deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatment_details');
+    }
+};
